16e4b3e852634372f49cbc5204b0a170e681aa3d
eklavya_15 <eklavya.lal@hotmail.com>
2024-06-21 02:31:07 +0530

    second commit

---
+++ server/routes/users.js
@@ -0,0 +1,40 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  getAllUsers,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+router.get("", verifyToken, getAllUsers);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
